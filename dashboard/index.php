5f1c7f409e056a63fa18f2e022b614e19d9728af
Miguel Ruiz <later481@gmail.com>
2022-01-12 14:38:22 -0600

    cambios formulario agregar

--- dashboard/index.php
+++ dashboard/index.php
@@ -61,12 +61,12 @@ $personal = $resultado->fetchAll(PDO::FETCH_ASSOC);
             <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                 <div class="sb-sidenav-menu">
                     <div class="nav">
-                        <div class="sb-sidenav-menu-heading">Menu</div>
+                        <div class="sb-sidenav-menu-heading">GENERAL</div>
                         <a class="nav-link" href="index.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Inicio
                         </a>
-                        <!--<div class="sb-sidenav-menu-heading">Interface</div>-->
+                        <div class="sb-sidenav-menu-heading">ADMINISTRACION</div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                             Empleados
@@ -74,7 +74,7 @@ $personal = $resultado->fetchAll(PDO::FETCH_ASSOC);
                         </a>
                         <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="addEmpleados.php">Agregar Empleado</a>
+                                <a class="nav-link" href="agregarEmpleados.php">Agregar Empleado</a>
                                 <a class="nav-link" href="verEmpleados.php">Ver Empleados</a>
                             </nav>
                         </div>
@@ -109,7 +109,7 @@ $personal = $resultado->fetchAll(PDO::FETCH_ASSOC);
                                 </div>
                             </nav>
                         </div>
-                        <!--<div class="sb-sidenav-menu-heading">Addons</div>-->
+                        <div class="sb-sidenav-menu-heading">NOMINAS</div>
                         <a class="nav-link" href="charts.html">
                             <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                             Charts
--- dashboard/vistas/superior.php
+++ dashboard/vistas/superior.php
@@ -51,12 +51,12 @@ if($_SESSION["s_usuario"] === null){
             <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                 <div class="sb-sidenav-menu">
                     <div class="nav">
-                        <div class="sb-sidenav-menu-heading">Menu</div>
+                        <div class="sb-sidenav-menu-heading">GENERAL</div>
                         <a class="nav-link" href="index.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Inicio
                         </a>
-                        <!--<div class="sb-sidenav-menu-heading">Interface</div>-->
+                        <div class="sb-sidenav-menu-heading">ADMINISTRACION</div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                             Empleados
@@ -64,8 +64,8 @@ if($_SESSION["s_usuario"] === null){
                         </a>
                         <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="layout-static.html">Agregar Empleado</a>
-                                <a class="nav-link" href="layout-sidenav-light.html">Ver Empleados</a>
+                                <a class="nav-link" href="agregarEmpleados.php">Agregar Empleado</a>
+                                <a class="nav-link" href="verEmpleados.php">Ver Empleados</a>
                             </nav>
                         </div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
@@ -99,12 +99,12 @@ if($_SESSION["s_usuario"] === null){
                                 </div>
                             </nav>
                         </div>
-                        <!--<div class="sb-sidenav-menu-heading">Addons</div>-->
+                        <div class="sb-sidenav-menu-heading">NOMINAS</div>
                         <a class="nav-link" href="charts.html">
                             <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                             Charts
                         </a>
-                        <a class="nav-link" href="tables.html">
+                        <a class="nav-link" href="personal.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                             Personal
                         </a>
@@ -117,10 +117,4 @@ if($_SESSION["s_usuario"] === null){
             </nav>
         </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
+        
