43559671364050e3afe6fdd02715e074825f2ec5
Miguel Ruiz <later481@gmail.com>
2022-01-24 12:46:25 -0600

    archivo import excel terminado

--- dashboard/bd-personal/importarExcel.php
+++ dashboard/bd-personal/importarExcel.php
@@ -9,13 +9,17 @@ if(isset($_POST['import'])){
     $allowedFileType=['application/vnd.ms-excel','text/xls','text/xlsx','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
   
     if(in_array($_FILES['file']['type'], $allowedFileType)){
+        //GUARDA EL ARCHIVO EN EL FICHERO
         $targetPath = 'subidas/'.$_FILES['file']['name'];
         //move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
         $excel = SimpleXLSX::parse(($_FILES['file']['tmp_name']));
         //echo "<pre>";print_r($excel->rows());
         //echo "archivo movido";
-        $sql = "INSERT INTO personal(id, num_empleado, nombre, ap_paterno, ap_materno, edad, fech_nacimiento, sexo, domicilio, estado, provincia, cod_postal, CURP, RFC, num_tel, correo, escolaridad, puesto) VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        //SE PREPARA LA SENTENCIA
+        $sql = "REPLACE INTO personal(id, num_empleado, nombre, ap_paterno, ap_materno, edad, fech_nacimiento, sexo, domicilio, estado, provincia, cod_postal, CURP, RFC, num_tel, correo, escolaridad, puesto) VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        //SE DECLARA VARIABLE CON LA SENTENCIA A OCUPAR
         $resultado = $conexion ->prepare($sql);
+        //SE DECLARA EL ORDEN DE LOS DATOS A TOMAR
         $resultado -> bindParam(1, $numEmpleado);
         $resultado -> bindParam(2, $nombre);
         $resultado -> bindParam(3, $aPaterno);
@@ -33,50 +37,32 @@ if(isset($_POST['import'])){
         $resultado -> bindParam(15, $correo);
         $resultado -> bindParam(16, $escolaridad);
         $resultado -> bindParam(17, $puesto);
-        
+        //ITERACION EN CADA UNA DE LAS COLUMNAS
         foreach ($excel->rows() as $key => $row) {
+             //echo"<pre>";print_r ($cells);
+             $numEmpleado = $row[0];
+             $nombre = $row[1];
+             $aPaterno = $row[2];
+             $aMaterno = $row[3];
+             $edad = $row[4];
+             $fNacimiento = $row[5];
+             $genero = $row[6];
+             $domicilio = $row[7];
+             $estado = $row[8];
+             $provincia = $row[9];
+             $cPostal = $row[10];
+             $curp = $row[11];
+             $rfc = $row[12];
+             $telefono = $row[13];
+             $correo = $row[14];
+             $escolaridad = $row[15];
+             $puesto = $row[16];
+        
             if($key != 0){
-                //echo"<pre>";print_r ($row);
-                //foreach ($row as $key => $cells) {
-                    //echo"<pre>";print_r ($cells);
-                    $numEmpleado = $row[0];
-                    $nombre = $row[1];
-                    $aPaterno = $row[2];
-                    $aMaterno = $row[3];
-                    $edad = $row[4];
-                    $fNacimiento = $row[5];
-                    $genero = $row[6];
-                    $domicilio = $row[7];
-                    $estado = $row[8];
-                    $provincia = $row[9];
-                    $cPostal = $row[10];
-                    $curp = $row[11];
-                    $rfc = $row[12];
-                    $telefono = $row[13];
-                    $correo = $row[14];
-                    $escolaridad = $row[15];
-                    $puesto = $row[16];
-                //}
-                if(!empty($numEmpleado)||!empty($curp)){
-                    $resultado->execute();
-                }
-                echo "<pre>"; print_r($numEmpleado);
+                
+                $resultado->execute();
             }
-            /*
-            foreach ($row as $key => $cells) {
-                $q="";
-                //echo"<pre>";print_r ($cells);
-                
-                
-                if($i==0){
-                    $q.=$cells."varchar(50)";
-                }else{
-                    $q.=$cells.",";
-                }
-                $i++;
-                echo $q; echo "<br>";
-                
-            }*/
+           
         }
     }
     header('location: ../verEmpleados.php');
