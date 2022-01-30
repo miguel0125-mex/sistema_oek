b6f5fa47a5a77ef001dd3d0ef1714a9cb0fb22f9
Miguel Ruiz <later481@gmail.com>
2022-01-29 21:32:31 -0600

    codigo importar txt dia 2

--- dashboard/bd-personal/import_asistencia.php
+++ dashboard/bd-personal/import_asistencia.php
@@ -1,12 +1,31 @@
 <?php
 include_once "conexion.php";
-
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+//CHECAR SI LA CONEXION ES EXISTOSA
+/*
+if($conexion){
+    echo "estas conectado";
+}*/
 $allowedFileType=['text/txt', 'text/csv', 'text/plain'];
 if(in_array($_FILES["file"]["type"],$allowedFileType)){
     $targetPath = 'subidas/'.$_FILES['file']['name'];
     move_uploaded_file($_FILES['file']['tmp_name'],$targetPath);
     echo "archivo movido";
+
+    $archivo = $targetPath;
+    //ABRIR EL ARCHIVO TXT
+    $archivo_open = fopen($archivo, 'r');
+
+    //SE RECORRE EL ARCHIVO
+    while(feof($archivo_open)==false){
+        $linea = fgets($archivo_open);
+        echo "<pre>"; print_r($linea);
+    };
+    //PREPARAR SENTENCIA SQL
+    //$t_sql = "";
 }
+fclose($archivo_open);
 //$nombre = ($_FILES['file']['name']);
 
 //echo ($nombre);
